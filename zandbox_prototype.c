d1774be54b61f6a94e01a62df5277e39520cf49b
zintopia <zintopia>
2024-02-17 11:06:48 -0300

    added more features

--- zandbox_prototype.c
+++ zandbox_prototype.c
@@ -6,18 +6,34 @@
 #define WIDTH 500
 #define HEIGHT 500
 
-#define sand_material_id 1
-#define rock_material_id 2 
-#define water_material_id 3
+#define empty_material_id (Uint8)0
+#define sand_material_id (Uint8)1
+#define rock_material_id (Uint8)2 
+#define water_material_id (Uint8)3
+#define oil_material_id (Uint8)4
 
+#define empty_color_scheme (SDL_Color){0, 0, 0, 255}
 #define sand_color_scheme (SDL_Color){245, 200, 100, 0.8}
 #define rock_color_scheme (SDL_Color){105, 105, 100, 0.8}
 #define water_color_scheme (SDL_Color){0, 65, 255, 0.8}
+#define oil_color_scheme (SDL_Color){55, 50, 10, 0.8}
+
+typedef struct{
+    Uint8 id;
+    SDL_Color color_scheme;
+    float density;
+}ptcl_t;
 
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
 
-int area[100][100] = {0};
+ptcl_t area[100][100] = {0};
+
+ptcl_t empty_p = {empty_material_id, empty_color_scheme, 0.f};
+ptcl_t sand_p = {sand_material_id, sand_color_scheme, 2.f};
+ptcl_t rock_p = {rock_material_id, rock_color_scheme, 2.f};
+ptcl_t water_p = {water_material_id, water_color_scheme, 1.f};
+ptcl_t oil_p = {oil_material_id, oil_color_scheme, 0.5f};
 
 void init_window(){
     SDL_Init(SDL_INIT_VIDEO);    
@@ -35,9 +51,21 @@ void init_window(){
     SDL_RenderPresent(renderer);
 }   
 
+int is_in_borders(int y, int x){
+    return (y >= 0 && y < 100 && x >= 0 && x < 100);
+}
 int is_empty(int y, int x){
-      return (area[y][x] == 0 && y >= 0 && y < 100
-                              && x >= 0 && x < 100); 
+      return (area[y][x].id == empty_material_id && is_in_borders(y, x)); 
+}
+
+ptcl_t get_particle_type(Uint8 id){
+    switch(id){
+        case sand_material_id: return sand_p; break;
+        case water_material_id: return water_p; break;
+        case rock_material_id: return rock_p; break;
+        case oil_material_id: return oil_p; break;
+        default: return empty_p; break;
+    }
 }
 
 void draw_particle(int y, int x, SDL_Color colors){
@@ -45,101 +73,199 @@ void draw_particle(int y, int x, SDL_Color colors){
     SDL_RenderDrawPoint(renderer, x, y);
 }
 
-int rand_value(){
+Uint8 rand_value(){
     return (rand() % 20);
 }
 
-void is_sand_in_water(int y, int x){
-    if(area[y+1][x] == water_material_id){
-        area[y+1][x] = 0;
-        area[y][x] = 0;
-        area[y+1][x] = sand_material_id;
-        area[y][x] = water_material_id;
+void powder_move_into_liquids(Uint8 y, Uint8 x){
+    ptcl_t referencep = get_particle_type(area[y][x].id);
+    Uint8 val = rand_value();
+    Uint8 d_left = is_in_borders(y+1, x-1);
+    Uint8 d_right = is_in_borders(y+1, x+1);
+
+
+    if(area[y][x].density > area[y+1][x].density){
+        ptcl_t targetp = area[y+1][x];
+        area[y][x] = empty_p;
+        area[y+1][x] = empty_p;
+        area[y+1][x] = referencep;
+        area[y][x] = targetp;
     }
-    else if(area[y+1][x-1] == water_material_id){
-        area[y+1][x-1] = 0;
-        area[y][x] = 0;
-        area[y+1][x-1] = sand_material_id;
-        area[y][x] = water_material_id;
+     else if(d_left && area[y][x].density > area[y+1][x-1].density && val <= 10){
+        ptcl_t targetp = get_particle_type(area[y+1][x-1].id);
+        area[y][x] = empty_p;
+        area[y+1][x-1] = empty_p;
+        area[y+1][x-1] = referencep;
+        area[y][x] = targetp;
     }
-    else if(area[y+1][x+1] == water_material_id){
-        area[y+1][x+1] = 0;
-        area[y][x] = 0;
-        area[y+1][x+1] = sand_material_id;
-        area[y][x] = water_material_id;
+     else if(d_right && area[y][x].density > area[y+1][x+1].density && val >= 10){
+        ptcl_t targetp = get_particle_type(area[y+1][x+1].id);
+        area[y][x] = empty_p;
+        area[y+1][x+1] = empty_p;
+        area[y+1][x+1] = referencep;
+        area[y][x] = targetp;
+    }
+}
+
+void liquid_move_into_density(Uint8 y, Uint8 x){
+    ptcl_t referencep = get_particle_type(area[y][x].id);
+    Uint8 val = rand_value();
+    Uint8 left = is_in_borders(y, x-1);
+    Uint8 right = is_in_borders(y, x+1);
+    Uint8 d_left = is_in_borders(y+1, x-1);
+    Uint8 d_right = is_in_borders(y+1, x+1);
+
+    if(area[y][x].density > area[y+1][x].density){
+        ptcl_t targetp = get_particle_type(area[y+1][x].id);
+        area[y][x] = empty_p;
+        area[y+1][x] = empty_p;
+        area[y+1][x] = referencep;
+        area[y][x] = targetp;
+    }
+    else if(d_left && area[y][x].density > area[y+1][x-1].density && val <= 10){
+        ptcl_t targetp = get_particle_type(area[y+1][x-1].id);
+        area[y][x] = empty_p;
+        area[y+1][x-1] = empty_p;
+        area[y+1][x-1] = referencep;
+        area[y][x] = targetp;
+    }
+     else if(d_right && area[y][x].density > area[y+1][x+1].density && val >= 10){
+        ptcl_t targetp = get_particle_type(area[y+1][x+1].id);
+        area[y][x] = empty_p;
+        area[y+1][x+1] = empty_p;
+        area[y+1][x+1] = referencep;
+        area[y][x] = targetp;
+    }
+    else if(left && area[y][x].density > area[y][x-1].density && val <= 10){
+        ptcl_t targetp = get_particle_type(area[y][x-1].id);
+        area[y][x] = empty_p;
+        area[y][x-1] = empty_p;
+        area[y][x-1] = referencep;
+        area[y][x] = targetp;
+    }
+    else if(right && area[y][x].density > area[y][x+1].density && val >= 10){
+        ptcl_t targetp = get_particle_type(area[y][x+1].id);
+        area[y][x] = empty_p;
+        area[y][x+1] = empty_p;
+        area[y][x+1] = referencep;
+        area[y][x] = targetp;
     }
 }
 
 void upt_sand(int y, int x){
-    int val = rand_value();
-    const int down = is_empty(y+1, x);
-    const int d_left = is_empty(y+1, x-1);
-    const int d_right = is_empty(y+1, x+1);
+    draw_particle(y, x, sand_color_scheme);
+
+    Uint8 val = rand_value();
+    Uint8 down = is_empty(y+1, x);
+    Uint8 d_left = is_empty(y+1, x-1);
+    Uint8 d_right = is_empty(y+1, x+1);
     
     if(down){
-        area[y][x] = 0;
-        area[y+1][x] = sand_material_id;
+        area[y][x] = empty_p;
+        area[y+1][x] = sand_p;
     }
     else if(d_left && val >= 10){
-        area[y][x] = 0;
-        area[y+1][x-1] = sand_material_id;
+        area[y][x] = empty_p;
+        area[y+1][x-1] = sand_p;
     }
     else if(d_right && val <= 10){
-        area[y][x] = 0;
-        area[y+1][x+1] = sand_material_id;
+        area[y][x] = empty_p;
+        area[y+1][x+1] = sand_p;
     }
-    else
-        is_sand_in_water(y, x);
+    else powder_move_into_liquids(y, x);
 }
 
 void upt_water(int y, int x){
-    int val = rand_value();
-    const int down = is_empty(y+1, x);
-    const int left = is_empty(y, x-1);
-    const int right = is_empty(y, x+1);
-    const int d_left = is_empty(y+1, x-1);
-    const int d_right = is_empty(y+1, x+1);
+    draw_particle(y, x, water_color_scheme);
+
+    Uint8 val = rand_value();
+    Uint8 down = is_empty(y+1, x);
+    Uint8 left = is_empty(y, x-1);
+    Uint8 right = is_empty(y, x+1);
+    Uint8 d_left = is_empty(y+1, x-1);
+    Uint8 d_right = is_empty(y+1, x+1);
     
     if(down){
-        area[y][x] = 0;
-        area[y+1][x] = water_material_id;
+        area[y][x] = empty_p;
+        area[y+1][x] = water_p;
     }
     else if(d_left && val <= 10){
-        area[y][x] = 0;
-        area[y+1][x-1] = water_material_id;
+        area[y][x] = empty_p;
+        area[y+1][x-1] = water_p;
     }
     else if(d_right && val >= 10){
-        area[y][x] = 0;
-        area[y+1][x+1] = water_material_id;
+        area[y][x] = empty_p;
+        area[y+1][x+1] = water_p;
     }
     else if(left && val <= 10){
-        area[y][x] = 0;
-        area[y][x-1] = water_material_id;
+        area[y][x] = empty_p;
+        area[y][x-1] = water_p;
     }
     else if(right && val >= 10){
-        area[y][x] = 0;
-        area[y][x+1] = water_material_id;
+        area[y][x] = empty_p;
+        area[y][x+1] = water_p;
+    }
+    else liquid_move_into_density(y, x);
+}
+
+void upt_oil(int y, int x){
+    draw_particle(y, x, oil_color_scheme);
+    Uint8 val = rand_value();
+    Uint8 down = is_empty(y+1, x);
+    Uint8 left = is_empty(y, x-1);
+    Uint8 right = is_empty(y, x+1);
+    Uint8 d_left = is_empty(y+1, x-1);
+    Uint8 d_right = is_empty(y+1, x+1);
+
+    if(down){
+        area[y][x] = empty_p;
+        area[y+1][x] = oil_p;
+    }
+    else if(d_left && val <= 10){
+        area[y][x] = empty_p;
+        area[y+1][x-1] = oil_p;
+    }
+    else if(d_right && val >= 10){
+        area[y][x] = empty_p;
+        area[y+1][x+1] = oil_p;
+    }
+    else if(left && val <= 10){
+        area[y][x] = empty_p;
+        area[y][x-1] = oil_p;
+    }
+    else if(right && val >= 10){
+        area[y][x] = empty_p;
+        area[y][x+1] = oil_p;
     }
 }
 
 void update_particles(){
     for(int y = 100-1; y>=0; y--){
         for(int x = 100-1; x>=0; x--){
-            switch(area[y][x]){
+            switch(area[y][x].id){
                 case sand_material_id:
-                    draw_particle(y, x, sand_color_scheme);
                     upt_sand(y, x);
                     break;
                 case water_material_id:
-                    draw_particle(y, x, water_color_scheme);
                     upt_water(y, x);
-                    break;;
+                    break;
+                case oil_material_id:
+                    upt_oil(y, x);
+                    break;
                 case rock_material_id:
-                    draw_particle(y, x, rock_color_scheme);
+                    draw_particle(y, x, rock_color_scheme); // doesn't create a particular instance in the matrix; to fix. (or not)
                     break;
             }
         }
     }
+}
+
+void place_particles(Uint8 y, Uint8 x, ptcl_t p){
+    area[y][x] = p;
+    area[y+1][x] = p;
+    area[y-1][x] = p;
+    area[y][x-1] = p;
+    area[y][x+1] = p;
 }
 
 int main(void){
@@ -149,9 +275,10 @@ int main(void){
     
     int running = 1;
     int count = 0;
-    int y, x;
-    int is_pressed = 0;
-    int id_reference = 1; /* initiates with sand as default. */
+    Uint16 y, x;
+    Uint8 is_right_pressed = 0;
+    Uint8 is_left_pressed = 0;
+    Uint8 id_reference = 1; /* initiates with sand as default. */
 
     while(running == 1){
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
@@ -163,32 +290,38 @@ int main(void){
                     running = 0;
                     break;
                 case SDL_MOUSEMOTION:
-                    int refy = event.motion.y/5, refx = event.motion.x/5;
-                    printf("%d\t%d\n", refy, refx);
-                    if(refy < 100 && refy >= 0 && refx < 100 && refx >= 0)  
+                    Uint16 refy = event.motion.y/5, refx = event.motion.x/5;
+                    
+                    if(refy < 99 && refy > 0 && refx < 99 && refx > 0)  
                         y = refy, x = refx;
-                    else is_pressed = 0; // mantains in 0.
+                    else is_left_pressed = 0; // mantains in 0.
                     break;
                 case SDL_MOUSEBUTTONDOWN:
                     if(event.button.button == SDL_BUTTON_LEFT)
-                        is_pressed = 1;     
+                        is_left_pressed = 1;
+                    if(event.button.button == SDL_BUTTON_RIGHT)
+                        is_right_pressed = 1; 
                     break;
                 case SDL_MOUSEBUTTONUP:
-                    is_pressed = 0;
+                    if(is_left_pressed)
+                        is_left_pressed = 0;
+                    else if(is_right_pressed)
+                        is_right_pressed = 0;
                     break;
                 case SDL_KEYDOWN:
                     if(event.key.keysym.sym == 'e')
                         id_reference++;
-                        if(id_reference > 3) id_reference = 1;
+                        if(id_reference > 4) id_reference = 1;
                     break;
             }
             
         }
-        if(area[y][x] == 0 && is_pressed){
-            printf("placed into area[%d][%d]\n", y, x);
-            if(count != 0 && count%2 == 0)
-                area[y][x] = id_reference;
-            count++;
+        if(is_left_pressed && area[y][x].id == empty_material_id){
+            ptcl_t p = get_particle_type(id_reference);
+            place_particles(y, x, p); 
+        }
+        else if(is_right_pressed){
+           place_particles(y, x, empty_p);
         }
         update_particles();
         SDL_RenderPresent(renderer);
